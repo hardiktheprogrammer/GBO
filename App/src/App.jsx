0e390c8fc69269253e63cfdf926bae7d57571436
Pritam Das <dpritam28577@gmail.com>
2022-12-22 23:29:13 +0530

    Carve text

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -3,15 +3,19 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import img from "./assets/UPDATED Giving Back Official PNG.png"
 import "./rotate.css"
+import Carved from "./components/Carved";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+    <Carved/>
       <div class="w-96 py-3.5 m-8">
-        <h1 class="text-amber-300">Welcome to Giving Back Official </h1>
-        <img className="square" class="bg-auto h-96 " src={img}></img>
+        <img class="square" src={img}></img>
+
       </div>
+
     </div>
   );
 }
---
+++ App/src/components/Carved.jsx
@@ -0,0 +1,24 @@
+import ReactCurvedText from "react-curved-text";
+
+const Carved = () => {
+  return (
+    <ReactCurvedText
+      width={915}
+      height={554}
+      cx={447}
+      cy={264}
+      rx={213}
+      ry={182}
+      startOffset={10}
+      reversed={true}
+      text="Welcome to Giving Back Official  "
+      textProps={{ style: { fontSize: 46 } }}
+      textPathProps={null}
+      tspanProps={-23}
+      ellipseProps={null}
+      svgProps={null}
+    />
+  );
+};
+
+export default Carved;
